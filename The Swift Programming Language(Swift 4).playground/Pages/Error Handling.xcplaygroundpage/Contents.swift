08ed55e04e53a1c40a1125f5dfac56b65877c33d
zhangshulin <zhangslwork@yeah.net>
2017-12-05 13:52:32 +0800

    Error Handing

---
+++ The Swift Programming Language(Swift 4).playground/Pages/Error Handling.xcplaygroundpage/Contents.swift
@@ -0,0 +1,142 @@
+//: [Previous](@previous)
+
+import Foundation
+
+
+enum VendingMachineError: Error {
+    case invalidSelection
+    case insufficientFunds(coinsNeeded: Int)
+    case outOfStock
+}
+
+//throw VendingMachineError.insufficientFunds(coinsNeeded: 5)
+
+
+struct Item {
+    var price: Int
+    var count: Int
+}
+
+class VendingMachine {
+    var inventory = [
+        "Candy Bar": Item(price: 12, count: 7),
+        "Chips": Item(price: 10, count: 4),
+        "Pretzels": Item(price: 7, count: 11)
+    ]
+    var coinDeposited = 0
+    
+    func vend(itemNamed name: String) throws {
+        guard let item = inventory[name] else {
+            throw VendingMachineError.invalidSelection
+        }
+        guard item.count > 0 else {
+            throw VendingMachineError.outOfStock
+        }
+        guard item.price <= coinDeposited else {
+            throw VendingMachineError.insufficientFunds(coinsNeeded: item.price - coinDeposited)
+        }
+        
+        coinDeposited -= item.price
+        
+        var newItem = item
+        newItem.count -= 1
+        inventory[name] = newItem
+        
+        print("Dispensing\(name)")
+    }
+}
+
+let favoriteSnacks = [
+    "Alice": "Chips",
+    "Bob": "Licorice",
+    "Eve": "Pretzels"
+]
+
+func buyFavoriteSnack(person: String, vendingMachine: VendingMachine) throws {
+    let snackName = favoriteSnacks[person] ?? "Candy Bar"
+    try vendingMachine.vend(itemNamed: snackName)
+}
+
+struct PurchasedSnack {
+    let name: String
+    init(name: String, vendingMachine: VendingMachine) throws {
+        try vendingMachine.vend(itemNamed: name)
+        self.name = name
+    }
+}
+
+var vendingMachine = VendingMachine()
+vendingMachine.coinDeposited = 8
+do {
+    try buyFavoriteSnack(person: "Alice", vendingMachine: vendingMachine)
+} catch VendingMachineError.invalidSelection {
+    print()
+} catch VendingMachineError.outOfStock {
+    print()
+} catch VendingMachineError.insufficientFunds(let coinsNeeds) {
+    print()
+}
+
+
+func someThrowingFunction() throws -> Int {
+    return 10
+}
+
+let x = try? someThrowingFunction()
+
+let y: Int?
+do {
+    y = try someThrowingFunction()
+} catch {
+    y = nil
+}
+
+func fetchDataFromDisk() throws -> String {
+    return "data"
+}
+
+func fetchDataFromServer() throws -> String {
+    return "data"
+}
+
+func fetchData() -> String? {
+    if let data = try? fetchDataFromDisk() { return data }
+    if let data = try? fetchDataFromServer() { return data }
+    return nil
+}
+
+func loadImage(atPath path: String) throws {
+    
+}
+
+let photo = try! loadImage(atPath: "./Resources/John Appleseed.jpg")
+
+//func processFile(filename: String) throws {
+//    if exists(filename) {
+//        let file = open(filename)
+//        defer {
+//            close(file)
+//        }
+//        while let line = try file.readline() {
+//            
+//        }
+//    }
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
